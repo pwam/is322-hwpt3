c65d9a0c67ab22994c2dc960c3b1c5d2119346e1
pwam <gardineerw@gmail.com>
2014-02-27 04:53:58 -0500

    Completed Image swipe to change image

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+	$(document).ready(function() {  
+  		$('#myCarousel').hammer().on('swipeleft', function(){
+  			$('#myCarousel').carousel('next'); 
+  		})
+  		$('#myCarousel').hammer().on('swiperight', function(){
+  			$('#myCarousel').carousel('prev'); 
+  		})
 
+	
+	}); 
